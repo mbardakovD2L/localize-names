d0b6dc22038a4f82e7a0788e43e438faf0f6b578
Max Bardakov <Maxim.Bardakov@D2L.com>
2022-11-14 14:11:18 -0500

    boilerplate code for creating and testing a module

---
+++ src/localize-names.js
@@ -0,0 +1,6 @@
+const helloIAmAFunction = () => {
+    console.log('I am a side effect');
+    return 'I am a return value';
+}
+
+export { helloIAmAFunction }
---
+++ test/unit-test.js
@@ -0,0 +1,4 @@
+import { helloIAmAFunction } from '../src/localize-names.js';
+    
+const res = helloIAmAFunction();
+console.log('result: ', res);
