d4dfa3f0aa9413dbc664d3574969d9437b0138b0
Max Bardakov <Maxim.Bardakov@D2L.com>
2022-11-15 10:03:35 -0500

    working prototype for english names

--- src/localize-names.js
+++ src/localize-names.js
@@ -3,4 +3,44 @@ const helloIAmAFunction = () => {
     return 'I am a return value';
 }
 
-export { helloIAmAFunction }
+// a name can have:
+// givenName, middleName, lastName, prefix, suffix, nickname
+// e.g. Dr. Harry "Stinky Man" Pepper Ham, PhD.
+// =>
+// givenName: Harry
+// middleName: Pepper
+// lastName: Ham
+// prefix: Dr.
+// suffix: PhD
+// nickname: Stinky Man
+
+const fieldsOrderMap = {
+    'en': [
+        'prefix', 'givenName', 'middleName', 'nickname', 'lastName', 'suffix'
+    ]
+}
+
+const localizeName = (nameBlob, locale) => {
+    switch (locale) {
+        case 'en':
+            return fieldsOrderMap[locale].map(field => helper(nameBlob, field, locale)).join(' ').replace(' , ', ', ');
+    }
+}
+
+const helper = (nameBlob, nameSubcomponent, locale) => {
+    switch (locale) {
+        case 'en':
+            switch (nameSubcomponent) {
+                case 'nickname':
+                    return `"${nameBlob.nickname}"`;
+                case 'suffix':
+                    return Object.keys(nameBlob).filter(part => fieldsOrderMap[locale].includes(part) && part !== 'suffix') ?
+                        `, ${nameBlob.suffix}` : nameBlob.suffix;
+                default:
+                    return nameBlob[nameSubcomponent];
+            }
+    }
+}
+
+// todo: pass each name fragment through a sub-formatter that itself handles things like the comma before the title, or the quotes around nickname
+export { localizeName }
--- test/unit-test.js
+++ test/unit-test.js
@@ -1,4 +1,16 @@
-import { helloIAmAFunction } from '../src/localize-names.js';
-    
-const res = helloIAmAFunction();
-console.log('result: ', res);
+import { localizeName } from '../src/localize-names.js';
+
+const testName = {
+    prefix: 'Dr.',
+    givenName: 'Harry',
+    middleName: 'Pepper',
+    lastName: 'Ham',
+    suffix: 'PhD',
+    nickname: 'Stinky Man'
+}
+
+const expectedName = 'Dr. Harry Pepper "Stinky Man" Ham, PhD'
+const resultName = localizeName(testName, 'en');
+if (expectedName !== resultName) {
+    console.error('TEST FAILED: expected ', expectedName, ', found: ', resultName);
+}
