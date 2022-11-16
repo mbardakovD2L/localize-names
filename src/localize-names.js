fa976b726a5bf63caebee6403b79deb76f814229
Max Bardakov <Maxim.Bardakov@D2L.com>
2022-11-16 10:39:04 -0500

    change fieldOrderMap to a function with a default

--- src/localize-names.js
+++ src/localize-names.js
@@ -1,22 +1,22 @@
 const givenFirst = [ 'prefix', 'givenName', 'middleName', 'nickname', 'surname', 'suffix' ];
 const surnameFirst = [ 'prefix', 'surname', 'givenName', 'middleName', 'nickname', 'suffix' ];
 
-const fieldsOrderMap = {
-	'en': givenFirst,
-	'ja': surnameFirst,
-	'ko': surnameFirst,
-	'vi': surnameFirst,
-	'yue': surnameFirst,
-	'zh': surnameFirst
+const fieldsOrderMap = (locale) => {
+	switch (locale){
+		case 'ja': return surnameFirst;
+		case 'ko': return surnameFirst;
+		case 'vi': return surnameFirst;
+		case 'yue': return surnameFirst;
+		case 'zh': return surnameFirst;
+		default: return givenFirst;
+	}
 }
 
 // https://github.com/unicode-org/cldr/blob/95363e0fbfe9a2be510dd4783bfd8f69aca07d85/common/main/en.xml#L9301
-// <nameOrderLocales order="givenFirst">und en</nameOrderLocales>
-// <nameOrderLocales order="surnameFirst">ja ko vi yue zh</nameOrderLocales>
 // TODO: maybe add a "level of formality" setting too? and a length? make it more analogous to above link?
 
 const localizeName = (nameBlob, locale) => {
-	return fieldsOrderMap[locale]
+	return fieldsOrderMap(locale)
 		.filter(locale => nameBlob[locale])
 		.map(field => processNamePart(nameBlob, field, locale))
 		.join(' ').replace(' , ', ', ');
@@ -25,12 +25,12 @@ const localizeName = (nameBlob, locale) => {
 
 const processNamePart = (nameBlob, nameSubcomponent, locale) => {
 	switch (locale) {
-		case 'en':
+		default:
 			switch (nameSubcomponent) {
 				case 'nickname':
 					return `"${nameBlob.nickname}"`;
 				case 'suffix':
-					return Object.keys(nameBlob).filter(part => fieldsOrderMap[locale].includes(part) && part !== 'suffix') ?
+					return Object.keys(nameBlob).filter(part => fieldsOrderMap(locale).includes(part) && part !== 'suffix') ?
 						`, ${nameBlob.suffix}` : nameBlob.suffix;
 				default:
 					return nameBlob[nameSubcomponent];
